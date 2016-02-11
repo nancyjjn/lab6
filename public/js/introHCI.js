47cd90ddc5dc5bcd31955ed92e1c63493ec3cb08
nancyjjn <nancyjjn@gmail.com>
2016-02-11 09:36:29 +0000

    finish lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,20 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	var url = '/project/' + idNumber;
+	console.log("URL is " + url);
+	$.get(url, getInfo);
+}
+
+function getInfo (result) {
+	console.log("Result is ", result);
+	var projectHTML = //'<a href="#" class = "thumbnail">'
+		'<img src = "' + result.image + '" class = "detailsImage">'
+		+ '<p><small>' + result.date + '</small></p>'
+		+ result['summary']; //+ '</a>';
+
+	$('#project' + result.id + ' .details').html(projectHTML);
 }
 
 /*
@@ -35,4 +49,18 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get('/palette', getColor);
+}
+
+function getColor (result) {
+	console.log("Result is ", result);
+	var colors = result.colors.hex;
+	console.log("Colors are ", colors);
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
